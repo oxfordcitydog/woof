b17018a539ee5d83468123d70ce027a53f5f6a39
Your Name <janeyatestheartist@gmail.com>
2024-01-06 20:12:58 +0000

    Initial commit

---
+++ streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    st.title("Streamlit Hello World App")
+    st.write("Hello, World!")
+    
+    # Add a subheading
+    st.subheader("Welcome to Streamlit!")
+
+if __name__ == "__main__":
+    main()
