34f528e112e308beb8f80edc44ab52b050958600
Your Name <janeyatestheartist@gmail.com>
2024-01-14 14:19:16 +0000

    Your commit message here

--- streamlit_elements.py
+++ streamlit_elements.py
@@ -1,32 +1,34 @@
 #this is a new file about exploring streamlit elements - first bring in the streamlit library
 import streamlit as st
 
-#st dot Displays text in Streamlit
+# Displays text in Streamlit
 st.text("Link to my Blog")
-# link to my blog
+
+# Link to my blog
 st.markdown('[OxfordCityDog.com](http://oxfordcitydog.com)')
 
-#to check code save it, then run it by hitting windows key + X, terminal. call the file and directory
-
-
 # Define the relative widths of the columns
-column_widths = [1, 2, 3]
-
-import streamlit as st
-from PIL import Image
+column_widths = [1, 1, 1]
 
 # Create a list to store uploaded photos
 uploaded_photos = []
 
 # Create three columns with specified widths
-col1, col2, col3 = st.columns([1, 2, 3])
+col1, col2, col3 = st.columns([1/3, 1/3, 1/3])
 
 # Add Markdown content to col1 (1/6 width) without showing the header
 col1.markdown('# Welcome to Woof!')
 col1.markdown('This is a woof-tastic page.')
 
 # Add Markdown content to col2 (1/3 width)
+col2.markdown('images by jane and AI')
 
+# Display the image
+# Image URL
+image_url = "https://i0.wp.com/oxfordcitydog.com/wp-content/uploads/2024/01/DALL%C2%B7E-2024-01-04-18.20.59-3d-rendered-of-a-yellow-balloon-dog-on-pale-green-background.png?resize=300%2C300&ssl=1"
+st.image(image_url, caption='Woof', use_column_width=True)
+
+#end of this bit of code
 
 # Upload a photo in col3 (1/2 width)
 new_photo = col3.file_uploader('Upload a photo of your dog 🐾', type=['jpg', 'png', 'jpeg'])
